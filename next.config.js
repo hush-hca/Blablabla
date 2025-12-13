73ad2a83239a70818cece9503a1c8dc19bf939b8
hush-hca <c4nnonworld@gmail.com>
2025-12-14 01:26:42 +0900

    fixed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,54 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // Server Components에서 특정 패키지를 외부화하여 번들에서 제외
+  serverComponentsExternalPackages: [
+    'pino',
+    'pino-pretty',
+    'thread-stream',
+  ],
+  // Webpack 설정으로 테스트 파일 제외
+  webpack: (config, { isServer }) => {
+    // thread-stream의 테스트 파일들을 무시
+    config.plugins = config.plugins || [];
+    const webpack = require('webpack');
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^thread-stream\/test/,
+      })
+    );
+    
+    // 테스트 파일 경로를 무시하도록 alias 설정
+    if (isServer) {
+      config.resolve = config.resolve || {};
+      config.resolve.alias = config.resolve.alias || {};
+      // 테스트 파일들을 false로 설정하여 무시
+      const testFiles = [
+        'thread-stream/test',
+        'thread-stream/bench',
+      ];
+      testFiles.forEach((file) => {
+        config.resolve.alias[file] = false;
+      });
+    }
+    
+    return config;
+  },
+  // Turbopack 설정 (Next.js 16)
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        'thread-stream/test': false,
+        'thread-stream/bench': false,
+      },
+    },
   },
 };
 
